81be7b7c6cb6b31d577de75dffd8260ae22b7148
Andrey Shostik <aaa73731919@gmail.com>
2017-05-22 15:40:01 +0300

    refactored checkAuth

--- src/components/checkAuth/accessUtils.js
+++ src/components/checkAuth/accessUtils.js
@@ -1,10 +1,4 @@
 export default function (requiredGroups, groups) {
-  const access = requiredGroups.some((group, index) => {
-    if (groups.indexOf(group) !== -1) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-  return access;
+  return requiredGroups.some(group => !!groups[group]);
 }
+
